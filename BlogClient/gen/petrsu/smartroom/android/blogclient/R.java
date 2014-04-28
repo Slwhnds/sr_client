9175932ba4ccdce5a39e860bdfda246205b49a07
1 <1@your-4c2a19a0bf>
2014-04-28 07:26:00 +0400

    authorization activity transfered

--- BlogClient/gen/petrsu/smartroom/android/blogclient/R.java
+++ BlogClient/gen/petrsu/smartroom/android/blogclient/R.java
@@ -10,6 +10,21 @@ package petrsu.smartroom.android.blogclient;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f040003;
+        public static final int clickedBtn=0x7f040009;
+        public static final int clickedSecBtn=0x7f04000a;
+        public static final int clickedTitleBtn=0x7f04000b;
+        public static final int clickedTitleSecBtn=0x7f04000c;
+        public static final int gray=0x7f040002;
+        public static final int lightblack=0x7f040000;
+        public static final int lightblue=0x7f040004;
+        public static final int staticBtn=0x7f040007;
+        public static final int staticSecBtn=0x7f040008;
+        public static final int staticTitleBtn=0x7f040005;
+        public static final int staticTitleSecBtn=0x7f040006;
+        public static final int white=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +32,58 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button=0x7f020000;
+        public static final int button_pressed=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int title_button=0x7f020003;
+        public static final int title_button_pressed=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f09000a;
+        public static final int connectBtn=0x7f090006;
+        public static final int editIP=0x7f090000;
+        public static final int editName=0x7f090003;
+        public static final int editPassword=0x7f090004;
+        public static final int editPort=0x7f090002;
+        public static final int guestBtn=0x7f090005;
+        public static final int guestEmail=0x7f090009;
+        public static final int guestName=0x7f090007;
+        public static final int guestPhone=0x7f090008;
+        public static final int regServiceBtn=0x7f090001;
     }
     public static final class layout {
         public static final int activity_authorization=0x7f030000;
+        public static final int guest_registration=0x7f030001;
     }
     public static final class menu {
-        public static final int authorization=0x7f070000;
+        public static final int authorization=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int connectBtn=0x7f060002;
+        public static final int connectionFail=0x7f06000e;
+        public static final int emptyFields=0x7f060014;
+        public static final int guestBtn=0x7f060003;
+        public static final int guestNameEmpty=0x7f06000f;
+        public static final int nameIsUsed=0x7f06000c;
+        public static final int participantEmail=0x7f060008;
+        public static final int participantLogin=0x7f060007;
+        public static final int participantPassword=0x7f06000a;
+        public static final int participantPhone=0x7f060009;
+        public static final int portFormatErr=0x7f06000b;
+        public static final int registrationFail=0x7f06000d;
+        public static final int registrationService=0x7f060004;
+        public static final int registrationTitle=0x7f060010;
+        public static final int ssIP=0x7f060005;
+        public static final int ssPort=0x7f060006;
+        public static final int userEmail=0x7f060013;
+        public static final int userName=0x7f060011;
+        public static final int userPhone=0x7f060012;
     }
     public static final class style {
         /** 
@@ -59,10 +107,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
