51a1712a863cabebb4c5ca845d80d321eed0b905
1 <1@your-4c2a19a0bf>
2014-05-09 13:22:43 +0400

    test project

---
+++ BlogClientTest/gen/petrsu/smartroom/android/blogclient/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package petrsu.smartroom.android.blogclient.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
