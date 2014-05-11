7c66122a6bb4502d9c7d224907442c549e1c6995
Admin <Admin@microsof-b22766>
2014-05-12 00:09:42 +0300

    empty test project for cameras created

---
+++ CameraClientTest/src/petrsu/smartroom/android/cameraclient/test/CameraTest.java
@@ -0,0 +1,19 @@
+package petrsu.smartroom.android.cameraclient.test;
+
+import junit.framework.TestCase;
+
+public class CameraTest extends TestCase {
+
+	public CameraTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
