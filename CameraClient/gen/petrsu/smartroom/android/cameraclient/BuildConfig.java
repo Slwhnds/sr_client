5c8cdb7c8bb17ebf7dd4a55074d296f41b7cce46
orifanne <orifanne@orifanne-N130>
2014-05-22 20:33:17 +0400

    deleted

--- CameraClient/gen/petrsu/smartroom/android/cameraclient/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package petrsu.smartroom.android.cameraclient;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
